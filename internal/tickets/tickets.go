9befec3a27d561e1714ff1a7833d46b1242b3ceb
ovacolmenares <osvaldo.colmenares@mercadolibre.com>
2022-12-29 17:37:57 -0300

    first commit

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,13 +1,103 @@
 package tickets
 
-type Ticket struct {
+import (
+	"encoding/csv"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Ticket struct { //TODO: Usar esta struct para mapear los datos
+	// ID      int
+	// Nombre  string
+	// Email   string
+	// Destino string
+	// Horario string
 }
 
-// ejemplo 1
-func GetTotalTickets(destination string) (int error) {}
+//var Tickets = make([]Ticket, 1000) //TODO: Definir un slice de tipo Ticket
 
-// ejemplo 2
-func GetMornings(time string) (int error) {}
+func GetTotalTickets(destination string) (int, error) {
+	file, err := os.Open("tickets.csv")
+	if err != nil {
+		return 0, errors.New("No se pudo abrir el archivo csv")
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.Comma = ','
+	records, _ := reader.ReadAll()
 
-// ejemplo 3
-func AverageDestination(destination string, total int) (int error) {}
+	cantidadViajes := 0
+	//TODO: Mapear aqui los datos hacia Ticket
+	for _, row := range records {
+		if row[3] == destination {
+			cantidadViajes++
+		}
+	}
+
+	return cantidadViajes, nil
+}
+
+func GetCountByPeriod() (int, int, int, int, error) {
+	// TODO: Utilizar el slice Ticket
+	file, err := os.Open("tickets.csv")
+	if err != nil {
+		return 0, 0, 0, 0, errors.New("No se pudo abrir el archivo csv")
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.Comma = ','
+	records, _ := reader.ReadAll()
+
+	madrugada := 0
+	maniana := 0
+	tarde := 0
+	noche := 0
+
+	for _, row := range records {
+		var hora = strings.Split(row[4], ":")
+		var horario, err = strconv.Atoi(hora[0])
+		if err != nil {
+			return 0, 0, 0, 0, errors.New("No se pudo convertir la hora a int")
+		}
+		switch {
+		case horario >= 0 && horario <= 6:
+			madrugada++
+		case horario >= 7 && horario <= 12:
+			maniana++
+		case horario >= 13 && horario <= 19:
+			tarde++
+		case horario >= 20 && horario <= 23:
+			noche++
+		default:
+			return 0, 0, 0, 0, errors.New("Ese horario no pertenece a ningún turno.")
+		}
+	}
+
+	return madrugada, maniana, tarde, noche, nil
+}
+
+func AverageDestination(destination string) (float64, error) {
+	// TODO: Utilizar el slice Ticket
+	file, err := os.Open("tickets.csv")
+	if err != nil {
+		return 0, errors.New("No se pudo abrir el archivo csv")
+	}
+	defer file.Close()
+	reader := csv.NewReader(file)
+	reader.Comma = ','
+	records, _ := reader.ReadAll()
+
+	c := 0
+	for _, row := range records {
+		if row[3] == destination {
+			c++
+		}
+	}
+
+	var cantidad float64 = float64(len(records))
+	var promedio float64 = (float64(c) / cantidad) * 100
+
+	return promedio, nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
 )
 
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("Se recuperó")
+		}
+		fmt.Println("ejecución finalizada")
+	}()
+
+	destino := "Brazil"
+	total, err := tickets.GetTotalTickets(destino)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Requerimiento 1")
+	fmt.Printf("%d personas viajaron a %s\n", total, destino)
+	fmt.Println("-------------")
+	madrugada, maniana, tarde, noche, err := tickets.GetCountByPeriod()
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Requerimiento 2")
+	fmt.Println("Madrugada:", madrugada)
+	fmt.Println("Mañana:", maniana)
+	fmt.Println("Tarde:", tarde)
+	fmt.Println("Noche:", noche)
+	fmt.Println("-------------")
+	total2, err := tickets.AverageDestination(destino)
+	fmt.Println("Requerimiento 3")
+	fmt.Println("Promedio:", total2, "%")
+	fmt.Println("-------------")
 }
